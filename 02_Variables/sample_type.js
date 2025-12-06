f428f68158e711bdd301e961404982875fdfdaf8
Chinmay Sai <yanamalachinmaysai106@gmail.com>
2025-12-06 17:13:10 +0530

    first commit

---
+++ 02_Variables/sample_type.js
@@ -0,0 +1,10 @@
+///Variables Example
+var found = true;
+var grade = 88.6;
+var firstName = "John";
+var lastName = 'Doe';
+var middleInitial = "A";
+console.log("Found:", found);
+console.log("Grade:", grade);
+console.log("Full Name:", firstName + " " + middleInitial + " " + lastName);
+console.log("Student Name: ".concat(firstName, " ").concat(middleInitial, " ").concat(lastName));
---
+++ 02_Variables/sample_type.ts
@@ -0,0 +1,13 @@
+///Variables Example
+
+
+let found : boolean = true;
+let grade : number = 88.6;
+let firstName : string = "John";
+let lastName : string = 'Doe';
+let middleInitial : string = `A`;
+
+console.log("Found:" , found);
+console.log("Grade:", grade);
+console.log("Full Name:"  , firstName + " " + middleInitial + " " + lastName);
+console.log(`Student Name: ${firstName} ${middleInitial} ${lastName}`);
