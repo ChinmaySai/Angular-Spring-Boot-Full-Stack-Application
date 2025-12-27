03cfd58afcd6ddb85c02bee86f54b669c7c092d8
Chinmay Sai <yanamalachinmaysai106@gmail.com>
2025-12-27 12:27:14 +0530

    Customer Class Commit

--- 04_classes/01_class_definition/Customer.ts
+++ 04_classes/01_class_definition/Customer.ts
@@ -2,15 +2,9 @@ class Customer
 {
     firstName: string;
     lastName: string;
-
-    constructor(theFirstName: string,theLastName: string)
-    {
-        this.firstName=theFirstName;
-        this.lastName=theLastName;
-    }
 }
 
-let customer=new Customer("Chinmay","Sai");
-//customer.firstName="Chinmay";
-//customer.lastName="Sai";
+let customer=new Customer();
+customer.firstName="Chinmay";
+customer.lastName="Sai";
 console.log("Customer Name: "+customer.firstName+" "+customer.lastName);
